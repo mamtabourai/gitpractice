fb4d603f488461b86d34f024e8243658590afc4c
Mamta <mamta.bourai2107@gmail.com>
2026-04-20 16:35:59 +0530

    seding to github

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "jannat" {
+  name     = "jannatrg"
+  location = "south india"
+}
+
+resource "azurerm_storage_account" "janstorage" {
+  name                     = "jannatstrg"
+  resource_group_name      = azurerm_resource_group.jannat.name
+  location                 = azurerm_resource_group.jannat.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "jannatcntr"
+  storage_account_name  = azurerm_storage_account.janstorage.name
+  container_access_type = "blob"
+}
